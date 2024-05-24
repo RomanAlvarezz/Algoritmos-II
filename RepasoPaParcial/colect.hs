c67feddedd47a4fd16accb538a92d8ee13105169
Roman <alvarezroman150@gmail.com>
2024-05-24 17:19:35 -0300

    colect ordenado

--- RepasoPaParcial/colect.hs
+++ RepasoPaParcial/colect.hs
@@ -1,12 +1,21 @@
 module Colect where
 
-l1 = [('a',3),('b',5),('a',9),('c',2),('b',7)]
+l1 = [('a',3),('c',2),('b',5),('a',9),('b',7)]
 
-collect [] = []
-collect ((l,n):xs) = (l , n:(listClave l xs)) : collect (borroClave l xs)
+collect xs = isort (collect' xs)
+
+collect' [] = []
+collect' ((l,n):xs) = (l , n:(listClave l xs)) : collect' (borroClave l xs)
 
 listClave _ [] = [] 
 listClave l ((c,v):xs) = if l == c then v : listClave l xs else listClave l xs
 
 borroClave _ [] = []
 borroClave l ((c,v):xs) = if l == c then borroClave l xs else (c,v) : borroClave l xs
+
+insert (c,v) [] = [(c,v)]
+insert (c,v) ((c',v'):xs) = if c < c' then (c,v):(c',v'):xs else (c',v'): insert (c,v) xs 
+
+isort [] = []
+isort [x] = [x]
+isort (x:xs) = insert x (isort xs)
